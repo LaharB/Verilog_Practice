167cb1bd57cfb163d55e78bb48a8c7b509503df8
LaharB <laharbaruah96@gmail.com>
2026-05-18 00:13:37 +0530

    Files Updated

--- Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/tb/tb_arithmetic.v
+++ Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/tb/tb_arithmetic.v
@@ -1,6 +1,13 @@
 module tb_arithmetic();
 
+    reg [3:0] a = 4'd9, b = 4'd4;
+    wire [4:0] sum, diff, div, mod;
+    wire [7:0] prod;
+
+    assign sum  = a + b;  // 9 + 4 = 13      
+    assign diff = a - b;  // 9 - 4 = 5
+    assign prod = a * b;  // 9 * 4 = 36
+    assign div  = a / b;  // 9 / 4 = 2 (integer division) 
+    assign mod  = a % b;  // 9 % 4 = 1 (remainder)
     
-
-
 endmodule

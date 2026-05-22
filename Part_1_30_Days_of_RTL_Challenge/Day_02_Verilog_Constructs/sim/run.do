5c7825ccb32a0bea4aacab9777f7d44d51591485
LaharB <laharbaruah96@gmail.com>
2026-05-23 00:07:11 +0530

    Files Updated

--- Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/sim/run.do
+++ Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/sim/run.do
@@ -1,6 +1,6 @@
 vlib work
 # vlog ../tb/tb_arithmetic.v
-# vlog ../tb/tb_logical.v
+vlog ../tb/tb_logical.v
 vsim -voptargs=+acc work.tb_logical
 add wave -r *
 run -all
--- Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/tb/tb_logical.v
+++ Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/tb/tb_logical.v
@@ -1,14 +1,15 @@
 module tb_logical();
 
     reg [3:0] x = 1100, y = 0000;
-    wire and_result, or_result, not_result;
+    wire and_result, or_result, not_result, not_result_2;
 
     assign and_result = x && y;  // 1 && 0 = 0
     assign or_result  = x || y;  // 1 || 0 = 1
     assign not_result = !x;      // !1 = 0
+    assign not_result_2 = !y;    // !0 = 1
 
     initial begin
-        $monitor("Values of x && y : %0b, x || y : %0b and !x : %0b", and_result, or_result, not_result);    
+        $monitor("Values of x && y : %0b, x || y : %0b and !x : %0b, !y : %0b", and_result, or_result, not_result, not_result_2);    
     end
 
 endmodule

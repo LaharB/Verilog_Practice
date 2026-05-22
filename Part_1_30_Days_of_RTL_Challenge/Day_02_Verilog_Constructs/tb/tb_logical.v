270ab0517fa25c94063f3b5f87f2db44f82e6a67
LaharB <laharbaruah96@gmail.com>
2026-05-22 23:56:32 +0530

    Files Updated

--- Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/tb/tb_logical.v
+++ Part_1_30_Days_of_RTL_Challenge/Day_02_Verilog_Constructs/tb/tb_logical.v
@@ -3,5 +3,12 @@ module tb_logical();
     reg [3:0] x = 1100, y = 0000;
     wire and_result, or_result, not_result;
 
+    assign and_result = x && y;  // 1 && 0 = 0
+    assign or_result  = x || y;  // 1 || 0 = 1
+    assign not_result = !x;      // !1 = 0
+
+    initial begin
+        $monitor("Values of x && y : %0b, x || y : %0b and !x : %0b", and_result, or_result, not_result);    
+    end
 
 endmodule
